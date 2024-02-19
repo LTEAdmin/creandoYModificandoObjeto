dfead688387db79bce6c885fe8e09a5231d7a449
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-19 20:42:24 -0300

    se ingresan datos de pacientes, luego se revisa por consolay el ingreso fue exitoso

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// creacion de objeto consultorio, con sus atributos, funciones get y set para cada uno de ellos
 function Consultorio(nombre, paciente) { 
     this.nombre = nombre;
     this.paciente = paciente;
@@ -19,6 +20,7 @@ function Consultorio(nombre, paciente) {
     };
 }
 
+// creacion de objeto paciente, con sus atributos, funciones get y set para cada uno de ellos
 function Paciente(nombre, rut, edad, diagnostico) { 
     this.nombre = nombre;
     this.rut = rut;
@@ -54,6 +56,19 @@ function Paciente(nombre, rut, edad, diagnostico) {
     Paciente.prototype.getDiagnostico = function () {
       return this.diagnostico;
     };
-
-    
 }
+
+// ingreso de datos de los pacientes
+let paciente1 = new Paciente("Maria", "1-9", 30, "covid");
+let paciente2 = new Paciente("juan", "2-7", 85, "neumonia");
+let paciente3 = new Paciente("pedro", "3-5", 25, "influenza");
+let paciente4 = new Paciente("luis", "4-3", 12, "asma");
+
+// ingreso de datos del consultorio
+let consultorio1 = new Consultorio("Consultorio 1", [paciente1, paciente2, paciente3, paciente4]);
+
+console.log(consultorio1); // hasta cargo correctamente a los pacientes en el consultorio
+
+//y otro método que permita mostrar todos los datos de los usuarios registrados.
+
+//Crear un método mediante la propiedad prototype que permita buscar los datos de los usuarios por nombre
